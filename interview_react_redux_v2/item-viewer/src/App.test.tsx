2d65d73d8dff6442cf8aeffada884b3e8fdcc6f8
Quy <20696858+KhanhQuy@users.noreply.github.com>
2026-05-01 12:03:22 +1000

    Fix App test to include Redux Provider

--- interview_react_redux_v2/item-viewer/src/App.test.tsx
+++ interview_react_redux_v2/item-viewer/src/App.test.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { store } from './store';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders Item Viewer heading', () => {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+
+  const headingElement = screen.getByText(/Item Viewer/i);
+  expect(headingElement).toBeInTheDocument();
 });
